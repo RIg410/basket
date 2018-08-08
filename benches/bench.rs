c011c1b74f05d17b3716b461c1f71facd6fe98bf
dyakushev <DYakushev@medlinesoft.ru>
2018-08-08 22:56:05 +0300

    split bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -21,13 +21,32 @@ fn make_put_data(price_count: usize, size_count: usize) -> Vec<(Price, Size, Met
     res
 }
 
+fn make_split_data(price_count: usize, size_count: usize) -> (Basket, (Price, Size)) {
+    let mut basket = Basket::new();
+    let data = make_put_data(price_count, size_count);
+
+    data.iter()
+        .for_each(|(price, size, meta)| {
+            basket.put(*price, *size, *meta)
+        });
+
+    let (price, _, _) = data[data.len() / 2];
+
+    let size = data.iter()
+        .filter(|(p, _, _)| *p < price)
+        .map(|(_, size, _)| size)
+        .sum();
+
+    (basket, (price, size))
+}
+
 fn put_test(mut basket: Basket, data: Vec<(Price, Size, Meta)>) {
     for call_item in data {
         basket.put(call_item.0, call_item.1, call_item.2);
     }
 }
 
-fn put_banch(c: &mut Criterion, price_count: usize, size_count: usize) {
+fn put_bench(c: &mut Criterion, price_count: usize, size_count: usize) {
     c.bench_function(&format!("put_test[price {0}, size: {1}]", price_count, size_count), move |b| {
         b.iter_with_large_setup(
             || (Basket::new(), make_put_data(price_count, size_count)),
@@ -39,11 +58,28 @@ fn put_banch(c: &mut Criterion, price_count: usize, size_count: usize) {
     });
 }
 
+fn split_bench(c: &mut Criterion, price_count: usize, size_count: usize) {
+    c.bench_function(&format!("split_test[price {0}, size: {1}]", price_count, size_count), move |b| {
+        b.iter_with_large_setup(
+            || make_split_data(price_count, size_count),
+            |(mut basket, split)| {
+                basket.split(split.0, split.1);
+            });
+    });
+}
+
 fn bench(c: &mut Criterion) {
-    put_banch(c, 100, 10);
-    put_banch(c, 100, 100);
-    put_banch(c, 1000, 10);
-    put_banch(c, 1000, 100);
+    put_bench(c, 100, 10);
+    split_bench(c, 100, 10);
+
+    put_bench(c, 100, 100);
+    split_bench(c, 100, 100);
+
+    put_bench(c, 1000, 10);
+    split_bench(c, 1000, 10);
+
+    put_bench(c, 1000, 100);
+    split_bench(c, 1000, 100);
 }
 
 criterion_group!(benches, bench);
